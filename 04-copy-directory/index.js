9d23de4d457357102783c2e980123d3191fcb935
Oleg Gordienov <ogordienov@yandex.ru>
2022-05-19 20:12:31 +0200

    feat: add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+async function copyDir() {
+  const newDir = path.join(__dirname, 'files-copy');
+  const initDir = path.join(__dirname, 'files');
+
+  try {
+    await fs.mkdir(newDir, {recursive: true});
+    const files = await fs.readdir(initDir);
+    for (const file of files) {
+      const newFile = path.join(newDir, file);
+      const initFile = path.join(initDir, file);
+      if ((await fs.stat(initFile)).isFile()) {
+        await fs.copyFile(initFile, newFile);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+copyDir();
